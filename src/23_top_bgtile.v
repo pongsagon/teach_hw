79f1531ee1e8efea6794d20348ccbbf64eab4288
Matt Pongsagon <mattdsik@hotmail.com>
2026-08-29 12:49:06 +0700

    Add files via upload

--- src/23_top_bgtile.v
+++ src/23_top_bgtile.v
@@ -46,15 +46,14 @@ module tt_um_vga_example (
 	wire [7:0] tile;
 	wire [5:0] color;
 
-	wire [9:0] x, y;
 	reg [9:0] scrollOffset = 0;
 	wire [10:0] x_scroll;
 
-	assign x_scroll = x + scrollOffset;
+	assign x_scroll = pix_x + scrollOffset;
 	
-	map_rom map_rom1(.clk(clk),.x_scroll(x_scroll[10:4]),.y(y),.tile(tile));
+	map_rom map_rom1(.clk(clk),.x_scroll(x_scroll[10:4]),.y(pix_y),.tile(tile));
 
-	bg_rom bg_rom1(.clk(clk),.tile(tile),.offsetX(x_scroll[3:0]),.offsetY(y[3:0]),.color(color));
+	bg_rom bg_rom1(.clk(clk),.tile(tile),.offsetX(x_scroll[3:0]),.offsetY(pix_y[3:0]),.color(color));
 
 	// handle input for scrolling, do oncex4 per frame
 	always @(posedge vsync) begin
